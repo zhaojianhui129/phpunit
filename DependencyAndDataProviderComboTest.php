ad17090aeffc3dd0cbe1635baef20dcd1b1c61b9
zhaojianhui129 <zhaojianhui@yaochufa.com>
2016-05-20 18:21:16 +0800

    更新单元测试练习代码

---
+++ DependencyAndDataProviderComboTest.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * 在同一个测试中组合使用 depends 和 dataProvider
+ */
+class DependencyAndDataProviderComboTest extends PHPUnit_Framework_TestCase
+{
+
+    public function provider()
+    {
+        return array(array('provider1'), array('provider2'));
+    }
+
+    public function testProducerFirst()
+    {
+        $this->assertTrue(true);
+        return 'first';
+    }
+
+    public function testProducerSecond()
+    {
+        $this->assertTrue(true);
+        return 'second';
+    }
+
+    /**
+     * @depends testProducerFirst
+     * @depends testProducerSecond
+     * @dataProvider provider
+     */
+    public function testConsumer()
+    {
+        $this->assertEquals(array('provider1', 'first', 'second'), func_get_args());
+    }
+}
