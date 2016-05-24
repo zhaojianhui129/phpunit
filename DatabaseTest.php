8f75500ea18964867ed27bec9523dd9dac7276ea
zhaojianhui129 <zhaojianhui@yaochufa.com>
2016-05-24 18:10:57 +0800

    更新测试代码

---
+++ DatabaseTest.php
@@ -0,0 +1,13 @@
+<?php
+class DatabaseTest extends PHPUnit_Framework_TestCase
+{
+    protected static $dbh;
+    
+    public static function setUpBeforeClass(){
+        self::$dbh = new PDO('sqlite::memory:');
+    }
+    
+    public static function tearDownAfterClass(){
+        self::$dbh = NULL;
+    }
+}
---
+++ Stack2Test.php
@@ -0,0 +1,29 @@
+<?php
+class Stack2Test extends PHPUnit_Framework_TestCase
+{
+    protected $stack;
+
+    protected function setUp()
+    {
+        $this->stack = array();
+    }
+
+    public function testEmpty()
+    {
+        $this->assertTrue(empty($this->stack));
+    }
+
+    public function testPush()
+    {
+        array_push($this->stack, 'foo');
+        $this->assertEquals('foo', array_pop($this->stack));
+        $this->assertTrue(empty($this->stack));
+    }
+
+    public function testPop()
+    {
+        array_push($this->stack, 'foo');
+        $this->assertEquals('foo', array_pop($this->stack));
+        $this->assertTrue(empty($this->stack));
+    }
+}
